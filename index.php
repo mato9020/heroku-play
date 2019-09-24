13b15659fadb7961505f3df15bf67f44c1ac6e56
= <matteson@udel.edu>
2019-09-23 20:36:27 -0400

    jnm

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
             $queryResult = $statement->fetchAll(PDO::FETCH_ASSOC);
             echo json_encode($queryResult);
             $queryResult = $queryResult.explode("@@");
-            echo json_encode("\n exploded query") + json_encode($queryResult);
+            echo json_encode($queryResult);
             array_push($wordResult, $queryResult);
             echo json_encode($wordResult);
         }
