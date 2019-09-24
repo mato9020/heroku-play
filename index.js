53865fbd45a4b2ce6391691856846da709f118ed
= <matteson@udel.edu>
2019-09-24 00:15:23 -0400

    a

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var genericGetRequest = function(){
    
 };
 $("#WordEntered").on('click',function() {
-    var enteredWord = $("#WordEntered").innerHTML;
+    var enteredWord = $("#GuessBox").innerHTML;
     
     var x = document.getElementById(enteredWord);
     if(x!=null){
